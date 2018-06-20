24418f523ce291f8fd189ebeac8c36c0fee2dc20
RRRLuxoft <rrr.lands@gmail.com>
2018-06-20 16:37:13 +0200

    4. Querying with Spring Data

--- src/main/java/com/example/university/domain/Staff.java
+++ src/main/java/com/example/university/domain/Staff.java
@@ -1,10 +1,6 @@
 package com.example.university.domain;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * JPA Entity representing a staff member of a department.
